ec40751b5651616f38b981cb7bd389ec7494b451
Maxim Vasilkin <mxvas@mail.ru>
2022-12-22 14:51:58 +0400

    task-3 resolved

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -6,12 +7,22 @@ def index(request):
 
 
 def show_catalog(request):
+    sort_by = request.GET.get('sort', 'name')
+    phones = list(Phone.objects.all())
+
+    if sort_by == 'name':
+        phones.sort(key=lambda x: x.name)
+    elif sort_by == 'min_price':
+        phones.sort(key=lambda x: x.price)
+    elif sort_by == 'max_price':
+        phones.sort(key=lambda x: x.price, reverse=True)
+
     template = 'catalog.html'
-    context = {}
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {'phone': Phone.objects.filter(slug=slug)[0]}
     return render(request, template, context)
